b155e3771c762eda88f09a0ee0e3f83813815f0e
Saad <saadmohammedanis@gmail.com>
2020-01-19 20:43:03 +0300

    Initial Commit

---
+++ Lister/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lister
+//
+//  Created by Saad Anis on 1/19/20.
+//  Copyright © 2020 Saad Anis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
